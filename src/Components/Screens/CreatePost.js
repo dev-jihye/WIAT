28ee3a897cf5aada407f06a94b070c626b23742e
Sophie <jihye340@gmail.com>
2022-02-09 03:28:35 +0900

    hover button color

--- src/Components/Screens/CreatePost.js
+++ src/Components/Screens/CreatePost.js
@@ -211,7 +211,7 @@ const CreatePost = () => {
           <div className="flex justify-end">
             <Link
               to={`/blog/${loggedInUser.uid}`}
-              className="bg-gray-200 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-0 focus:ring-offset-2 "
+              className="bg-gray-200 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-0 focus:ring-offset-2 "
             >
               Cancel
             </Link>
--- src/Components/Screens/EditPost.js
+++ src/Components/Screens/EditPost.js
@@ -244,7 +244,7 @@ const CreatePost = () => {
               <div className="flex justify-end">
                 <Link
                   to={`/blog/${loggedInUser.uid}`}
-                  className="bg-w py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-0 focus:ring-offset-2 "
+                  className="bg-w py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-0 focus:ring-offset-2 "
                 >
                   Cancel
                 </Link>
